ea9ffcd4a6acc5a81f4e80e6802e3999f3a3338d
Brenno Lemos <dev@brenno.codes>
2025-06-11 15:06:02 -0300

    custom: replace minimap plugin with better alternative

--- lua/custom/plugins/mini_map.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'echasnovski/mini.map',
-  commit = 'ecbb9b1e2223a04171388887765e7695f5a0c132',
-  config = function()
-    local MiniMap = require 'mini.map'
-    MiniMap.setup()
-    vim.keymap.set('n', '<Leader>mt', MiniMap.toggle)
-  end,
-}
