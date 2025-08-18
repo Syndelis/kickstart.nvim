9d1a19410bfb4c8ea7fe2152298ad11f300f7dca
Brenno Lemos <dev@brenno.codes>
2025-08-18 14:36:15 -0300

    custom: remove tab completion on function parameters

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -63,6 +63,8 @@ return {
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
+        -- Custom: Disable <Tab> jumping around
+        ['<Tab>'] = {},
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
