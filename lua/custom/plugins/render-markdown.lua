8731df2f718c0bb93dfb05461a35b5a0a277be49
Brenno Lemos <dev@brenno.codes>
2025-09-05 10:20:48 -0300

    custom: markdown rendering plugin

---
+++ lua/custom/plugins/render-markdown.lua
@@ -0,0 +1,10 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  commit = 'e76eb2e4262f0f0a1a7bd7a454dd7d44f1299afd',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' }, -- if you use standalone mini plugins
+  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {},
+}
